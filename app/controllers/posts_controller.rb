ef2f74f6a625d30581d5b9238e659dc0118e21c5
Nick Gray <nickjoegray@gmail.com>
2013-06-12 22:42:09 -0400

    add Posts Controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all 
+  end
+
+  def create
+    @post = Post.new params[:post]
+    if @post.save
+      flash[:success] = "You have spread kindness!"
+      redirect_to [:root]
+    else
+      render 'static_pages/home'
+    end
+  end
+
+end
