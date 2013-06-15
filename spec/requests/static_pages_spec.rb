b6a0d032346efeef02349b37bc14682da4dafdc7
Nick Gray <nickjoegray@gmail.com>
2013-06-15 03:33:16 -0400

    refactor rspec test code

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,72 +3,65 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
+    before { visit '/static_pages/home' }
 
     it "should have the h1 'Kindness Changes Everything'" do
-      visit '/static_pages/home'
       page.should have_selector('h1', :text => "Kindness Changes Everything")
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
       page.should have_selector('title', :text => "Kindness Changes Everything")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
       page.should_not have_selector('title', :text => "| Home")
     end
   end
 
   describe "Support page" do
+    before { visit '/static_pages/support' }
 
     it "should have the h1 'Support'" do
-      visit '/static_pages/support'
       page.should have_selector('h1', :text => "Support")
     end
 
     it "should have the base title" do
-      visit '/static_pages/support'
       page.should have_selector('title', :text => "Kindness Changes Everything")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/support'
       page.should_not have_selector('title', :text => "| Support")
     end
   end
 
   describe "About page" do
+    before { visit '/static_pages/about' }
 
     it "should have the h1 'About'" do
-      visit '/static_pages/about'
       page.should have_selector('h1', :text => "About")
     end
 
     it "should have the base title" do
-      visit '/static_pages/about'
       page.should have_selector('title', :text => "Kindness Changes Everything")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/about'
       page.should_not have_selector('title', :text => "| About")
     end
   end
 
 describe "Blog page" do
-  it "should have the h1 'Blog'" do
-      visit '/static_pages/blog'
+  before {visit '/static_pages/blog' }
+
+    it "should have the h1 'Blog'" do
       page.should have_selector('h1', :text => "Blog")
     end
 
     it "should have the base title" do
-      visit '/static_pages/blog'
       page.should have_selector('title', :text => "Kindness Changes Everything")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/blog'
       page.should_not have_selector('title', :text => "| Blog")
     end
   end
