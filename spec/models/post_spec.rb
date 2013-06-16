06ba61ed20341bd64359de2f2420c32947a14d0e
Nick Gray <nickjoegray@gmail.com>
2013-06-16 14:58:05 -0400

    add tests for content in spec/models/post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,4 +7,16 @@ describe Post do
   it { should respond_to(:content) }
 
   before { @post = Post.new(content: "Lorem ipsum") }
+
+  describe "with blank content" do 
+    before { @post.content = " " }
+    it { should_not be_valid }
+  end
+
+  describe "with content that is too long" do 
+    before { @post.content = "b" * 151 }
+    it { should_not be_valid }
+  end
+  
 end
+
